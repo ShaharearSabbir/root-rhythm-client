4b26bb2038818d4844568d884a9d046a45f24232
ShaharearSabbir <imshaharear@gmail.com>
2025-05-20 13:18:09 +0600

    project initialized

---
+++ src/Layouts/HomeLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Components/Header";
+
+const HomeLayout = () => {
+  return (
+    <div>
+      <div className="bg-base-200 shadow-md">
+        <Header></Header>
+      </div>
+    </div>
+  );
+};
+
+export default HomeLayout;
